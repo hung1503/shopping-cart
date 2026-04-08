9581351bf65b6baa418a4b0d379e2a8ccb79994d
hung1503 <hungnguyen150300@gmail.com>
2026-04-08 23:45:46 +0300

    Adding style to navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div>
-      <p>created by Hung Nguyen</p>
+    <div className="absolute bottom-0 left-0 right-0 bg-gray-600 text-white h-16 flex items-center justify-center">
+      <p>created by @Hung Nguyen</p>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,28 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div>
-      <h2>Shop</h2>
-      <div>
-        <a href="/">Homepage</a>
-        <a href="/products">Products</a>
-        <a href="/about">About</a>
-        <a href="/cart">Cart</a>
+    <nav className="relative bg-yellow-300">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <h2 className="text-gray-800 text-lg font-bold hover:text-gray-500 cursor-pointer">
+            Shop
+          </h2>
+          <div className="flex items-center justify-between gap-4 text-gray-800">
+            <a className="hover:text-gray-500" href="/">
+              Homepage
+            </a>
+            <a className="hover:text-gray-500" href="/products">
+              Products
+            </a>
+            <a className="hover:text-gray-500" href="/about">
+              About
+            </a>
+            <a className="hover:text-gray-500" href="/cart">
+              Cart
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
