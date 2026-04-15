ed19f6c7dec75c13e2de55e490fec90e5994624c
hung1503 <hungnguyen150300@gmail.com>
2026-04-15 22:13:37 +0300

    Adding simple about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <h2 className="m-4 font-medium text-xl">About us!</h2>
+      <p className="m-4 text-center">
+        Hi, thank you for visiting our shopping website! e are dedicated to
+        providing the best services and experiences for our customers. Our
+        mission is to bring innovation and creativity to everything we do
+      </p>
+    </div>
+  );
 };
