27881f6f25a29247b0c72fd82591254af9e21f44
hung1503 <hungnguyen150300@gmail.com>
2026-04-29 21:37:19 +0300

    Adding simple cart info

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const CartPage = () => {
-  return <div>CartPage</div>;
+  const [cart, setCart] = useState([]);
+  return (
+    <div className="flex flex-row justify-between p-10 gap-4">
+      <div>
+        <h1>In your cart</h1>
+        {cart.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          cart.map((item, index) => (
+            <div key={index}>
+              <div>
+                <h3>{item.name}</h3>
+                <img
+                  src={item.images[0]}
+                  alt={item.title}
+                  className="w-50 h-50 bg-cover bg-center bg-no-repeat"
+                />
+              </div>
+              <div>
+                <p>Quantity</p>
+                <p>{item.price}</p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      <div>
+        <h1>Your total order</h1>
+      </div>
+    </div>
+  );
 };
